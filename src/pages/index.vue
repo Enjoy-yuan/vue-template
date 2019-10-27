9fbfee52da6c344a58b827eb30d58c5737523604
袁程 <yuancheng@192.168.0.104>
2019-10-27 21:54:48 +0800

    2131

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -52,7 +52,7 @@
         </div>
         <div class="item button-rainbow" style="--bg-color: #e74c3c;">
           <button>
-            关于我333
+            关于我333444
             <div class="rainbow"></div>
           </button>
           <div class="name">Rainbow</div>
