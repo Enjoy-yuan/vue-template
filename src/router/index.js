452cac84900c68a6dd0a453acd04558bd91e24d0
袁程 <yuancheng@192.168.0.103>
2019-11-09 00:33:53 +0800

    123123

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/pages'
+import Music from '@/pages/music'
+import Movie from '@/pages/movie'
+import Book from '@/pages/book'
+import Photo from '@/pages/photo'
+
 
 Vue.use(Router)
 
@@ -8,27 +14,32 @@ export default new Router({
   routes: [{
       path: '/',
       name: 'Home',
-      component: () => import(/* webpackChunkName: "home" */ '@/pages/index.vue')
+      component: Home
+      // () => import(/* webpackChunkName: "home" */ '@/pages/index.vue')
     },
     {
       path: '/photo',
       name: 'Photo',
-      component: () => import(/* webpackChunkName: "photo" */ '@/pages/Photo/index.vue')
+      component: Photo
+      // () => import(/* webpackChunkName: "photo" */ '@/pages/Photo/index.vue')
     },
     {
       path: '/music',
       name: 'Music',
-      component: () => import(/* webpackChunkName: "music" */  '@/pages/Music/index.vue')
+      component: Music
+      // () => import(/* webpackChunkName: "music" */  '@/pages/Music/index.vue')
     },
     {
       path: '/movie',
       name: 'Movie',
-      component: () => import(/* webpackChunkName: "movie" */  '@/pages/Movie/index.vue')
+      component: Music
+      // () => import(/* webpackChunkName: "movie" */  '@/pages/Movie/index.vue')
     },
     {
       path: '/book',
       name: 'Book',
-      component: () => import(/* webpackChunkName: "book" */  '@/pages/Book/index.vue')
+      component: Music
+      // () => import(/* webpackChunkName: "book" */  '@/pages/Book/index.vue')
     }
   ]
 })
