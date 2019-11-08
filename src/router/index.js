b7893d3bd7da94146c3c516620d6f6cb47c11bea
袁程 <yuancheng@192.168.0.103>
2019-11-09 00:14:54 +0800

    73643646

--- src/router/index.js
+++ src/router/index.js
@@ -8,27 +8,27 @@ export default new Router({
   routes: [{
       path: '/',
       name: 'Home',
-      component: () => import(/* webpackChunkName: "home" */ '@/pages')
+      component: () => import(/* webpackChunkName: "home" */ '@/pages/index.vue')
     },
     {
       path: '/photo',
       name: 'Photo',
-      component: () => import(/* webpackChunkName: "photo" */ '@/pages/Photo')
+      component: () => import(/* webpackChunkName: "photo" */ '@/pages/Photo/index.vue')
     },
     {
       path: '/music',
       name: 'Music',
-      component: () => import(/* webpackChunkName: "music" */  '@/pages/Music')
+      component: () => import(/* webpackChunkName: "music" */  '@/pages/Music/index.vue')
     },
     {
       path: '/movie',
       name: 'Movie',
-      component: () => import(/* webpackChunkName: "movie" */  '@/pages/Movie')
+      component: () => import(/* webpackChunkName: "movie" */  '@/pages/Movie/index.vue')
     },
     {
       path: '/book',
       name: 'Book',
-      component: () => import(/* webpackChunkName: "book" */  '@/pages/Book')
+      component: () => import(/* webpackChunkName: "book" */  '@/pages/Book/index.vue')
     }
   ]
 })
