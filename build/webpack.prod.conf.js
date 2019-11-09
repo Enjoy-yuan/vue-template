9ff41747070ed2fd542a4f7c59b2e950bd78b9e0
袁程 <yuancheng@192.168.0.103>
2019-11-09 13:31:09 +0800

    开启gzip压缩

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -125,7 +125,8 @@ if (config.build.productionGzip) {
 
   webpackConfig.plugins.push(
     new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
+      // asset: '[path].gz[query]',
+      filename: '[path].gz[query]',
       algorithm: 'gzip',
       test: new RegExp(
         '\\.(' +
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
-    <keep-alive>
-      <router-view/>
-    </keep-alive>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
