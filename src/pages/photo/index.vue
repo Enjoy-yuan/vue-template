1ebbbf2d10ff49d029d8a50dc5c52c0249b39bdc
袁程 <yuancheng@192.168.0.103>
2019-11-09 15:54:13 +0800

    加图

--- src/pages/photo/index.vue
+++ src/pages/photo/index.vue
@@ -8,16 +8,46 @@
     <img src="@/assets/img/6.jpg" alt />
     <img src="@/assets/img/7.jpg" alt />
     <img src="@/assets/img/8.jpg" alt />
+    <img src="@/assets/img/9.jpg" alt />
+    <img src="@/assets/img/10.jpg" alt />
+    <img src="@/assets/img/11.jpg" alt />
+    <img src="@/assets/img/12.jpg" alt />
+    <img src="@/assets/img/13.jpg" alt />
+    <img src="@/assets/img/14.jpg" alt />
+    <img src="@/assets/img/15.jpg" alt />
+    <img src="@/assets/img/16.jpg" alt />
+    <img src="@/assets/img/17.jpg" alt />
+    <img src="@/assets/img/18.jpg" alt />
+    <img src="@/assets/img/19.jpg" alt />
+    <img src="@/assets/img/20.jpg" alt />
+    <img src="@/assets/img/21.jpg" alt />
+    <img src="@/assets/img/22.jpg" alt />
+    <img src="@/assets/img/23.jpg" alt />
+    <img src="@/assets/img/24.jpg" alt />
+    <img src="@/assets/img/25.jpg" alt />
+    <img src="@/assets/img/26.jpg" alt />
+    <img src="@/assets/img/27.jpg" alt />
+    <img src="@/assets/img/28.jpg" alt />
+    <img src="@/assets/img/29.jpg" alt />
+    <img src="@/assets/img/30.jpg" alt />
+    <img src="@/assets/img/31.jpg" alt />
+    <img src="@/assets/img/32.jpg" alt />
+
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      imgUrl:"@/assets/img/1.jpg"
+    };
   }
 };
 </script>
 
 <style scoped>
+img {
+  width: 100%;
+}
 </style>
