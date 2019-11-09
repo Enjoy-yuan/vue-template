ce58a3be7500105987668ea94e429d90cd787130
袁程 <yuancheng@192.168.0.103>
2019-11-09 15:22:53 +0800

    开启图片压缩

--- src/pages/photo/index.vue
+++ src/pages/photo/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-photo
+    <img src="../../assets/img/1.jpg" alt="">
+    <img src="../../assets/img/2.jpg" alt="">
+    <img src="../../assets/img/3.jpg" alt="">
+    <img src="../../assets/img/4.jpg" alt="">
+    <img src="../../assets/img/5.jpg" alt="">
+    <img src="../../assets/img/6.jpg" alt="">
+    <img src="../../assets/img/7.jpg" alt="">
+    <img src="../../assets/img/8.jpg" alt="">
+
+
   </div>
 </template>
 
