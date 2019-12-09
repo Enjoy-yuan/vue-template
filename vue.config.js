5f385a6f43d2be10982810d649386a7bc6f343f7
袁程 <yuancheng@192.168.0.104>
2019-12-09 21:27:39 +0800

    45646

--- vue.config.js
+++ vue.config.js
@@ -1,31 +1,31 @@
-// const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
 module.exports = {
-//   productionSourceMap: false, // build时不生成.map文件
-//   configureWebpack: config => {
-//     // 配置CDN加速
-//     config.externals = {
-//       vue: "Vue",
-//       "vue-router": "VueRouter",
-//       axios: "axios",
-//       "element-ui": "ELEMENT",
-//       Vuex: "Vuex"
-//     };
-//     // 生产环境才生效
-//     if (process.env.NODE_ENV === "production") {
-//       // 配置gzip压缩
-//       // config.plugins.push(
-//       //   new CompressionWebpackPlugin({
-//       //     // 正在匹配需要压缩的文件后缀
-//       //     test: /\.(js|css|svg|woff|ttf|json|html)$/,
-//           // 大于10kb的会压缩
-//           // threshold: 10240
-//           // 删除原文件
-//           // deleteOriginalAssets: true
-//         // })
-//       // );
-//     }
-//   },
+  productionSourceMap: false, // build时不生成.map文件
+  configureWebpack: config => {
+    // 配置CDN加速
+    config.externals = {
+      vue: "Vue",
+      "vue-router": "VueRouter",
+      axios: "axios",
+      "element-ui": "ELEMENT",
+      Vuex: "Vuex"
+    };
+    // 生产环境才生效
+    if (process.env.NODE_ENV === "production") {
+      配置gzip压缩
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          // 正在匹配需要压缩的文件后缀
+          test: /\.(js|css|svg|woff|ttf|json|html)$/,
+          // 大于10kb的会压缩
+          threshold: 10240,
+          // 删除原文件
+          // deleteOriginalAssets: true
+        })
+      );
+    }
+  },
   devServer: {
     // 设置api跨域代理
     proxy: {
