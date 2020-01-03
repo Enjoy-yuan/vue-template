43b305f6c74dfd147feea8360c11cb07be30a7ed
1244026470 <1244026470@qq.com>
2020-01-03 13:42:50 +0800

    464567

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
+    <h3>
+      从全端到全栈
+    </h3>
     <ul>
-      <li>商城前台：vue+vue-router+axios+vuex+elementui</li>
-      <li>商城管理：vue+vue-router+axios+vuex+elementui</li>
-      <li>商城API：node+mysql</li>
+      <li>商城前台：vue+elementui+nuxt</li>
+      <li>商城管理：vue+elementui+vue-cli3.x</li>
+      <li>商城API：node+mysql/mongodb+nest</li>
+      <li>商城小程序及APP：uniapp</li>
     </ul>
   </div>
 </template>
