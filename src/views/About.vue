3a39332e8f7e21c39ce70604240201b629f8b2bb
yuancheng <1244026470@qq.com>
2020-02-06 18:21:40 +0800

    34567890

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,10 +4,11 @@
       从全端到全栈
     </h3>
     <ul>
-      <li>商城前台：vue+elementui+nuxt</li>
-      <li>商城管理：vue+elementui+vue-cli3.x</li>
-      <li>商城API：node+mysql/mongodb+nest</li>
-      <li>商城小程序及APP：uniapp</li>
+      <li>前台页面：vue+elementui+nuxt</li>
+      <li>后台管理：vue+elementui+vue-cli3</li>
+      <li>API：node+mysql/mongodb</li>
+      <li>多端小程序：uniapp</li>
+      <li>原生APP：react native</li>
     </ul>
   </div>
 </template>
