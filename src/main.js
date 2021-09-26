ed0c2da1eb0a4fb41d6ce73224564d33ebe9bf4a
yuancheng <yuancheng@mucang.cn>
2021-09-26 14:58:34 +0800

    fix: 注册启动子应用从main.js中迁出

--- src/main.js
+++ src/main.js
@@ -17,8 +17,6 @@ import 'echarts/theme/macarons.js'
 // import  'echarts/theme/chalk.js'
 import { getCookie } from '@/utils/cookie'
 
-// 使用qiankun
-import { registerMicroApps, start } from 'qiankun'
 // 调用npm自定义组件
 import Button from 'yc-vue-common/components/Button'
 Vue.use(Button)
@@ -46,47 +44,6 @@ Vue.use(ElementUI, {
 })
 
 Vue.config.productionTip = false
-
-// 子应用列表
-let apps = []
-
-if (process.env.NODE_ENV === 'development') {
-  apps = [
-    {
-      name: 'microVue2',
-      entry: '//localhost:8080', // 子应用的地址，这里演示是本地启动的地址。
-      container: '#container', // 子应用的容器节点的选择器（vue一般为app）
-      activeRule: '/Micro/Vue2' // 访问子应用的规则，比如：主应用为localhost:8081，那访问该子应用的url应为localhost:8081/subapp
-    },
-    {
-      name: 'microVue3',
-      entry: '//localhost:8081', // 子应用的地址，这里演示是本地启动的地址。
-      container: '#container', // 子应用的容器节点的选择器（vue一般为app）
-      activeRule: '/Micro/Vue3' // 访问子应用的规则，比如：主应用为localhost:8081，那访问该子应用的url应为localhost:8081/subapp
-    }
-  ]
-} else if (process.env.NODE_ENV === 'production') {
-  apps = [
-    {
-      name: 'microVue2',
-      entry: 'https://vue2.yuanchengjs.cn/', // 子应用的地址，这里演示是本地启动的地址。
-      container: '#container', // 子应用的容器节点的选择器（vue一般为app）
-      activeRule: '/Micro/Vue2' // 访问子应用的规则，比如：主应用为localhost:8081，那访问该子应用的url应为localhost:8081/subapp
-    },
-    {
-      name: 'microVue3',
-      entry: 'https://vue3.yuanchengjs.cn/', // 子应用的地址，这里演示是本地启动的地址。
-      container: '#container', // 子应用的容器节点的选择器（vue一般为app）
-      activeRule: '/Micro/Vue3' // 访问子应用的规则，比如：主应用为localhost:8081，那访问该子应用的url应为localhost:8081/subapp
-    }
-  ]
-}
-
-// 注册子应用
-registerMicroApps(apps)
-
-// 启动
-start()
 
 new Vue({
   router,
